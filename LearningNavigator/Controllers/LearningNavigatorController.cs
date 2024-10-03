fabd76a2e2ab40b405170cb73d644508f60a17fe
Dot Net By Vickypedia <vickyraj.501@gmail.com>
2024-10-03 21:03:48 +0530

    Intial Commit

---
+++ LearningNavigator/Controllers/LearningNavigatorController.cs
@@ -0,0 +1,33 @@
+﻿using LearningNavigator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LearningNavigator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LearningNavigatorController : ControllerBase
+    {
+        private LearningNavigatorModel learningNavigatorModel;
+        /// <summary>
+        /// Constructor of LearningNavigatorController
+        /// </summary>
+        public LearningNavigatorController(LearningNavigatorModel model)
+        {
+            learningNavigatorModel = model;
+        }
+
+        public List<LearningNavigatorModel> GetLearningNavigatorModels()
+        {
+            var result = new List<LearningNavigatorModel>();
+            result.Add(new LearningNavigatorModel()
+            {
+                Id = 1,
+                Category = "IT",
+                SectionName = "A",
+                CourseName = "learn database"
+            });
+            return result;
+        }
+    }
+}
---
+++ LearningNavigator/Models/LearningNavigatorModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningNavigator.Models
+{
+    public class LearningNavigatorModel
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+
+        public string SectionName { get; set; }
+
+        public string Category { get; set; }
+    }
+}
